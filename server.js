2b195a686d1099780a2cd654ca2cae37bf22eb48
JoelHauser <joel.hauser@gmail.com>
2022-06-09 22:26:45 -0400

    update

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
